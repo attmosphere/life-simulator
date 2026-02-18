bd0dd038e5bfa1c5323b5a2d6c7327a75fd636b2
Kayke Moreira <srkayke4@gmail.com>
2026-02-18 17:21:21 -0300

    Added options menu, dynamic user input and required translation texts for the input

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import time
 
 lang_dict = {
     "en": {
+        "menu": """
+        CHOOSE YOUR OPTION:
+        1 - Create new person
+        2 - Kill person
+        3 - Show population
+        """,
+        "kill": "Who do you want to kill? ",
+        "type_name": "Type the person's name: ",
+        "no_numbers": "No numbers allowed",
+        "type_age": "Type the age: ",
+        "only_numbers": "Only numbers allowed",
+        "no_person": "No person called {} was found",
         "death_quotes": ["I have no regrets.",
                         "I wish I had more time.",
                         "I wish I had more time with my loved ones.",
@@ -20,9 +32,22 @@ lang_dict = {
         "init": "Person named {}, age {} got created!",
         "str": "Name: {}\nAge: {}\nAlive: {}",
         "worker_str": "\nJob: {}\nSalary: {}",
-        "die_message": "{}\n{} has died at the age of {} :(\n\"{}\""
+        "die_message": "{}\n{} has died at the age of {} :(\n\"{}\"",
+        "age_msg": "\r\n{} has aged by {} years"
     },
     "pt": {
+        "menu": """
+        ESCOLHA SUA OPÇÃO:
+        1 - Criar nova pessoa
+        2 - Matar pessoa
+        3 - Exibir população
+        """,
+        "type_name": "Digite o nome da pessoa: ",
+        "no_numbers": "Números não são permitidos",
+        "type_age": "Digite a idade: ",
+        "only_numbers": "Apenas números são permitidos",
+        "no_person": "Nenhuma pessoa chamada {} foi encontrada",
+        "kill": "Quem você quer matar? ",
         "death_quotes": ["Não tenho arrependimentos.",
                         "Gostaria de ter tido mais tempo.",
                         "Gostaria de ter tido mais tempo com meus entes queridos.",
@@ -39,21 +64,27 @@ lang_dict = {
         "init": "Pessoa chamada {}, idade {} foi criada!",
         "str": "Nome: {}\nIdade: {}\nVivo: {}",
         "worker_str": "\nTrabalho: {}\nSalário: {}",
-        "die_message": "{}\n{} morreu aos {} anos :(\n\"{}\""
+        "die_message": "{}\n{} morreu aos {} anos :(\n\"{}\"",
+        "age_msg": "\r\n{} envelheceu {} anos"
     }
 }
 class Person:
+    humans = []
     def __init__(self, name, age): # constructor
         self.name = name
         self.age = age
         self.alive = True
         self.life_expectancy = random.randint(70, 100)
+        Person.humans.append(self)
         print(lang_choice["init"].format(name,age))
     def __str__(self):
         return lang_choice["str"].format(self.name, self.age, self.alive)
     def get_older(self, years):
         if self.alive:
             self.age+=years
+            print(lang_choice["age_msg"].format(self.name, years))
+            if self.age == 60:
+                print(lang_choice["grandpa_message"].format(self.name))
             if self.roll_death_dice():
                 self.die()
         else:
@@ -62,48 +93,69 @@ class Person:
         self.alive = False
         print(lang_choice["die_message"].format("*"*10,self.name, self.age, random.choice(lang_choice["death_quotes"])))
     def roll_death_dice(self):
-        death_chance = math.pow(self.age/self.life_expectancy, 8) 
+        death_chance = pow(self.age/self.life_expectancy, 8) 
         death_chance = min(death_chance, 0.95) # cap the death chance at 95%
         if random.random() < death_chance:
             return True
         else:
             return False
-
+    def auto_age(self):
+        while self.alive:
+            time.sleep(5) # ages every 5 seconds
+            self.get_older(1) 
 class Worker(Person):
     def __init__(self, name, age, job, salary):
         super().__init__(name, age)
         self.job = job
         self.salary = salary
-    def __str__(self): # overriding the __str__ method of the parent class
+    def __str__(self): # operator overload, overriding the __str__ method of the parent class
         text = super(Worker, self).__str__() + lang_choice["worker_str"].format(self.job, self.salary)
         return text
-
+def findPerson(name):
+    for person in Person.humans:
+        if person.name == name and person.alive:
+            return person 
+    return False # if we don't find the person in the loop, we return False, meaning we didn't find them
 print("PICK YOUR LANGUAGE / ESCOLHA SEU IDIOMA")
-lang = input("English (en) / Português (pt): ").lower()
-if lang != "en" and lang != "pt":
-    print("Invalid language, defaulting to English.")
+lang = input("English (en) / Português (pt): ").lower() # PT, pT ou Pt -> pt
+if lang != "en" and lang != "pt":                       # EN, eN ou En -> en
+    print("Invalid option, defaulting to English.")
     lang = "en"
 lang_choice = lang_dict[lang] # automatically gets the respective language key in the language *dictionary
-name = str(input(lang_choice["enter_name"]))
-age = int(input(lang_choice["enter_age"]))
-person1 = Person(name, age)
-print(person1)
-while input(lang_choice["get_older"]) != "n" and person1.alive: 
-    person1.get_older(random.randint(1,3))
-    print(person1)
-print(lang_choice["goodbye"].format(person1.name))
-print("\n\n\n")
-time.sleep(1.5)
-if input(lang_choice["create_worker"]).lower() == "n": exit()
-print(lang_choice["create_worker_message"])
-time.sleep(1.5)
-worker1 = Worker("Alice", 30, "Software Engineer", 2500) # monthly salary
-print(worker1,"\nLife expectancy:",worker1.life_expectancy)
-while worker1.alive:
-    # print(worker1)
-    print("Age: {}".format(worker1.age))
-    if worker1.age > 60 and worker1.age <= 67:
-        print(lang_choice["grandpa_message"].format(worker1.name))
-        time.sleep(1)
-    worker1.get_older(random.randint(1,3))
-    time.sleep(0.25)
+while True:
+    print(lang_choice["menu"])
+    choice = input("> ")
+    try:
+        choice=int(choice)
+    except:
+        break # not int
+    if choice == 1:
+        name=input(lang_choice["type_name"])
+        try:
+            int(name)
+            print(lang_choice["no_numbers"])
+            break
+        except:
+            if not name.isalpha(): # verify if name is alphanumeric (a-Z)
+                print(lang_choice["no_numbers"])
+                break
+        age=input(lang_choice["type_age"])
+        try:
+            age=int(age)
+        except:
+            print(lang_choice["only_numbers"])
+            break
+        Person(name, age)
+    elif choice == 2:
+        name = input(lang_choice["kill"])
+        if not isinstance(name, str):
+            exit()
+        p = findPerson(name)
+        if p:
+            p.die()
+        else:
+            print(lang_choice["no_person"].format(name))
+    elif choice == 3:
+        for human in Person.humans:
+            if human.alive:
+                print(human)
